6b75a9bf0fd25144d2bc9c393f4a6c19a6d97bf1
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2024-12-14 00:15:30 -0800

    In progress day 13

--- day13/kotlin/src/main/kotlin/Main.kt
+++ day13/kotlin/src/main/kotlin/Main.kt
@@ -1,7 +1,33 @@
 fun main(args: Array<String>) {
-    println("Hello World!")
+    if (args.isNotEmpty() && args.size == 2 && args[0] == "--part") {
+        val input = generateSequence(::readLine).joinToString("\n")
+        when (args[1]) {
+            "1" -> part1(input)
+            "2" -> part2(input)
+            else -> println("Invalid part number. Use 1 or 2.")
+        }
+    } else {
+        println("Usage: --part <1|2>")
+    }
+}
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+// Button A: X+94, Y+34
+// Button B: X+22, Y+67
+// Prize: X=8400, Y=5400
+
+val pattern = Regex(
+    """
+    Button A: X\+(\d+), Y\+(\d+)
+    Button B: X\+(\d+), Y\+(\d+)
+    Prize: X=(\d+), Y=(\d+)
+    """.trimIndent()
+)
+fun part1(input: String) {
+    pattern.findAll(input).sumOf { match ->
+        val (aX, aY, bX, bY, pX, pY) = match.destructured
+        1.0
+    }.let(::println)
+}
+
+fun part2(input: String) {
 }
